ba1d10fc3748e8a57277fe1d9e880292646f0f31
Nihil Patel <nihilpatel@10.1.24.191>
2015-09-16 11:56:02 -0500

    Working on referee

--- src/Referee.java
+++ src/Referee.java
@@ -10,6 +10,16 @@ public class Referee {
 	 * constructor - set up the variables for the Referee class
 	 */
 	// TODO: write the Referee's constructor.
+	private int playerMoney;
+	private int winnings;
+	private int payment;
+	
+	public Referee()
+	{
+		playerMoney = 100;
+		winnings = 0;
+		payment = 1;
+	}
 	
 	/**
 	 * playGame - the main game loop.
@@ -17,7 +27,26 @@ public class Referee {
 	public void playGame()
 	{
 		// TODO: write the Referee's playGame method.
+		Machine testMech = new Machine();
 		System.out.println("Setting Up Game...");
+		while (playerMoney>0)
+		{
+		
+		testMech.spin();
+		winnings = testMech.calculatePayout() - payment;
+		System.out.println(testMech.toString());
+		if(winnings>=0)
+		{
+			System.out.println("You won:"+winnings);
+		}
+		else
+		{
+			System.out.println("You lost" +winnings);
+		}
+		playerMoney += winnings;
+		System.out.println("You have "+playerMoney+" chips remaining.");
+		}
+		System.out.println("Game Over!");
 		
 	}
 }
